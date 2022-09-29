f50b314f4f3cad2d14cc839fa663def015a8a6e8
PPProsenjite <prosenjitpappu03455@gmial.com>
2022-09-29 06:22:48 +0600

    card position,color fixed

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './Category.css'
 const Category = (props) => {
     // console.log(props.category)
-    const{picture, name, about,age_gap, time} = props.category;
+    const { picture, name, about, age_gap, time } = props.category;
     return (
         <div className='category'>
             <img src={picture} alt="" />
-            <h5>{name}</h5>
-            <p>{about}</p>
-            <h5>Age for: {age_gap}</h5>
-            <h5>Time Required: {time}s</h5>
+            <div className='category-info'>
+                <h5>{name}</h5>
+                <p>{about}</p>
+                <h5>Age for: {age_gap}</h5>
+                <h5>Time Required: {time}s</h5>
+            </div>
             <button className='btn-add'>Add List</button>
         </div>
     );
